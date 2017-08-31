e087355b63e0c6aee530cb9037a3137c87dabb39
David Bristow <Prepress@David-Bristows-iMac.local>
2017-08-31 16:11:52 -0500

    setting up websockets

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express')
 const dbInfo = require('./databaseInfo.js')
-const app = express()
+var app = require('express')();
+var http = require('http').Server(app);
 const port = process.env.PORT || 4000;
 const mysql      = require('mysql');
 const connection = mysql.createConnection(dbInfo);
-var io = require('socket.io')(app);
+var io = require('socket.io')(http);
+
 
 
 
@@ -12,17 +13,25 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
- 
-connection.connect();
- 
+io.on('connection', function (socket) {
+
+    console.log('a client connected');
+
 connection.query('SELECT * FROM users;', function (error, results, fields) {
   if (error) throw error;
   console.log(results);
+      socket.emit('showrows', results);
+    })
 });
- 
-connection.end();
 
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port)
-})
+
+ 
+
+
+// app.listen(port, function () {
+//   console.log('Example app listening on port ' + port)
+// })
